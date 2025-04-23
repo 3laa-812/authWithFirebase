353132b8c44c0246960de55b78fd94fcb380e0ea
3laa-812 <3laa.r.812@gmail.com>
2025-04-23 19:38:45 +0200

    .

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -27,7 +27,7 @@ const Dashboard = () => {
         <Card.Body>
           <h2 className="text-center mb-4">Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser && currentUser.email}
+          <strong>Email: </strong> {currentUser && currentUser.email}
           <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
             Update Profile?
           </Link>
